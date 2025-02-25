ce83f19144fb650fb0ad612d0cd8cc5a43e1c7d5
Gustavo Lage <gustavo.lage.dev@gmail.com>
2025-02-25 17:49:10 -0300

    Adding files from JQuery learning

--- JQuery_aula/modulo_9_estrutura_inicial/main.js
+++ JQuery_aula/modulo_9_estrutura_inicial/main.js
@@ -1,16 +1,28 @@
 $(document).ready(function(){
-    console.log(document.querySelector('header button'))
-    console.log($('#cancel-button'))
-
-    document.querySelector('header button').addEventListener('click', function(e){
-
+    $('header button').click(function(){
+        $('form').slideDown();
     })
 
-    $('header button').click(function(){
-        alert("Exepandir formulário")
+    $('#cancel-button').click(function(){
+        $('form').slideUp();
     })
 
     $('form').on('submit',function(e){
         e.preventDefault();
+
+        const urlNewImg = $('#url-new-image').val();
+        const newItem = $('<li style="display:none"> </li>');
+
+        $(`<img src = "${urlNewImg}" >`).appendTo(newItem);
+        $(`
+            <div class = "overlay-img-link">
+                <a href = "${urlNewImg}" target = "_blank" title = "Ver imagem em tamanho real">
+                    Ver imagem em tamanho real
+                <a/>
+            </div>
+        `).appendTo(newItem);
+        $(newItem).appendTo('ul');
+        $(newItem).fadeIn(1000);
+        $('#url-new-image').val('');
     })
-})
+})   
