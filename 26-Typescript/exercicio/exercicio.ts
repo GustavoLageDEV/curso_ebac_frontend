40ef39d4d6ae3801d7060191f736421f41ba00a6
Gustavo Lage <gustavo.lage.dev@gmail.com>
2025-06-10 10:29:55 -0300

    Commiting TS exercise

---
+++ 26-Typescript/exercicio/exercicio.ts
@@ -0,0 +1,11 @@
+function multiplica(num1:number,num2:number): number {
+    return num1 * num2
+}
+
+function saudacao(nome:string):string {
+    return `Olá ${nome}`
+}
+
+console.log(multiplica(5,8));
+
+console.log(saudacao('Gustavo'));
