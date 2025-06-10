69eeaa9deffc2c7e946501cd8e43abaa96eba280
Gustavo Lage <gustavo.lage.dev@gmail.com>
2025-06-10 10:34:03 -0300

    Merging TS exercise into Main

---
+++ 26-Typescript/exercicio/exercicio.js
@@ -0,0 +1,9 @@
+"use strict";
+function multiplica(num1, num2) {
+    return num1 * num2;
+}
+function saudacao(nome) {
+    return `Olá ${nome}`;
+}
+console.log(multiplica(5, 8));
+console.log(saudacao('Gustavo'));
