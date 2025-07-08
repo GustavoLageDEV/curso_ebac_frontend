61efcf479955377e560108d8a9fd5d70c2161ef7
Gustavo Lage <gustavo.lage.dev@gmail.com>
2025-07-08 12:14:15 -0300

    React + CSS modules lesson + exercise Guthub Perfil

--- 28-react/projeto-react/src/components/Perfil/index.jsx
+++ 28-react/projeto-react/src/components/Perfil/index.jsx
@@ -1,16 +1,13 @@
-import "./perfil.css"
+import styles from './Perfil.module.css';
 
-const Perfil = () => {
-    const usuario = {
-        nome: "Gustavo Lage",
-        avatar: 'https://github.com/GustavoLageDEV.png'
-    } 
-
+const Perfil = ({ nomeUsuario }) => {
     return (
-        <div>
-            <img className="perfil-avatar" src={usuario.avatar}/>
-            <h3 className="perfil-titulo">{usuario.nome}</h3>
-        </div>
+        <header className={styles.header}> 
+            <img className={styles.avatar} src={`https://github.com/${nomeUsuario}.png`} />
+            <h1 className={styles.name} >
+                {nomeUsuario}
+                </h1>
+        </header>
     )
 }
 
